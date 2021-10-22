b37dd4766bd4e0f6e7f5dccacc7f538c2b1348ed
JossueVega <jossue.vega99@outlook.com>
2021-10-21 22:27:43 -0700

    Modificacion

---
+++ reactproyec/src/Componentes/Header.js
@@ -0,0 +1,12 @@
+//import logotipo2 from '..Componentes';
+import '../App.css';
+
+const Header = () => {
+    return (  
+      <header className="App-header">
+        <img src={'https://e7.pngegg.com/pngimages/186/863/png-clipart-apple-logo-apple-logo-computer-wallpaper.png'} className="App-logo" alt="logo" />
+      </header>
+    );
+}
+ 
+export default Header;
